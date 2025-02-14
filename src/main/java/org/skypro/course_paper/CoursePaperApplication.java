f1d76c5f99d943e31117ddbbed9c270cefbf20f8
VoT <alexxaski@yandex.ru>
2025-02-14 17:38:34 +0300

    br

---
+++ src/main/java/org/skypro/course_paper/CoursePaperApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.course_paper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePaperApplication.class, args);
+		System.out.println("приветы");
+	}
+}
---
+++ src/test/java/org/skypro/course_paper/CoursePaperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.course_paper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
