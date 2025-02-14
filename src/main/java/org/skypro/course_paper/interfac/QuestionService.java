6d8a7991f60ed34ad8919e55e4394fa6a89a3e0f
VoT <alexxaski@yandex.ru>
2025-02-14 23:12:54 +0300

    br02

--- src/main/java/org/skypro/course_paper/interfac/QuestionService.java
+++ src/main/java/org/skypro/course_paper/interfac/QuestionService.java
@@ -3,6 +3,7 @@ package org.skypro.course_paper.interfac;
 import org.skypro.course_paper.Question;
 import java.util.List;
 
+
 public interface QuestionService {
     void addQuestion(Question question);
     void removeQuestion(String questionText);
--- src/test/java/org/skypro/course_paper/JavaQuestionServiceTest.java
+++ src/test/java/org/skypro/course_paper/JavaQuestionServiceTest.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 @ExtendWith(MockitoExtension.class)
 public class JavaQuestionServiceTest {
 
